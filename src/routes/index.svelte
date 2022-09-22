e844f92993a7724bc417f44e10620f442c0f637d
Lucas Florio <lucasefe@gmail.com>
2022-09-22 13:43:29 -0300

    Add reviews ann fix webchat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,16 @@
 <svelte:head>
 	<title>Home</title>
+  <script defer async src="https://chat.broadly.com/javascript/chat.js"
+data-broadly-chat-id="632c7d2e682ccb4d08efb8b8"></script>
+  <script defer async src="https://embed.broadly.com/include.js"
+data-url="/632c7d2e682ccb4d08efb8b8/reviews"
+data-embed-id="broadly-reviews-container"></script>
 </svelte:head>
 
 <section>
 	<h1>
     Scrapbooklandia
 	</h1>
+  <div id="broadly-reviews-container"></div>
 </section>
-
-<script defer async src="https://chat.broadly.com/javascript/chat.js"
-data-broadly-chat-id="632c7d2e682ccb4d08efb8b8"></script>
 
